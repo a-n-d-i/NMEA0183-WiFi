3f768dc8fe5c5af04617364a9b466d090a03fadf
Andi <Andi>
2017-04-28 12:33:47 +0200

    initial commit

---
+++ NMEA0183-WiFi.ino
@@ -0,0 +1,99 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include "SoftwareSerial.h"
+#include <WiFiUdp.h>
+#include "credentials.h"
+#include <Ticker.h>
+
+/*
+ * Some soft Watchdog functionality taken from 
+ * https://github.com/esp8266/Arduino/issues/1532
+ */
+Ticker tickerOSWatch;
+
+#define OSWATCH_RESET_TIME 30
+
+static unsigned long last_loop;
+
+void ICACHE_RAM_ATTR osWatch(void) {
+    unsigned long t = millis();
+    unsigned long last_run = abs(t - last_loop);
+    if(last_run >= (OSWATCH_RESET_TIME * 1000)) {
+      // save the hit here to eeprom or to rtc memory if needed
+        ESP.restart();  // normal reboot 
+        //ESP.reset();  // hard reset
+    }
+}
+
+/* we have to use softserial because ttl level from out nmea source is inverted compared to 
+the rs232 level of the esp8266 UART. Might be a good idea to use a level converter and the 
+build in UART instead of a voltage divider and softserial. Already put in an order for parts...
+*/
+SoftwareSerial softserial(D1, 3, true); // RX, TX, inverted
+
+const char* ssid = SSID;
+const char* password = PASSWORD;
+
+String currentSentence;
+
+WiFiUDP Udp;
+
+ESP8266WebServer server(80);
+
+// handles http request to esp8266
+void handleRoot() {
+  server.send(200, "text/plain", "last sentence " + currentSentence);
+}
+
+void setup(void){
+  last_loop = millis();
+  tickerOSWatch.attach_ms(((OSWATCH_RESET_TIME / 3) * 1000), osWatch);
+
+  pinMode(LED_BUILTIN, OUTPUT); 
+  Serial.begin(4800);
+  softserial.begin(4800);
+  Serial.println("startup....");
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  MDNS.begin("esp8266wifi");
+  server.on("/", handleRoot);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+char databuffer[300];
+
+void loop(void){
+  server.handleClient();
+  digitalWrite(LED_BUILTIN, LOW);
+  softserial.setTimeout(30);
+
+  // just listen on the serial and send every line which starts
+  // with a "$" as an UDP Packet...
+  if (softserial.available() > 0 ) {
+    byte count = softserial.readBytesUntil(0x0A, databuffer, 300);
+    if (databuffer[0] == '$') {
+      last_loop = millis();
+      currentSentence = String(databuffer);
+      Serial.println(currentSentence);
+      Udp.beginPacket(HOST, PORT);
+      Udp.write(databuffer);
+      Udp.endPacket();
+      digitalWrite(LED_BUILTIN, HIGH);
+    }   
+  }
+}
